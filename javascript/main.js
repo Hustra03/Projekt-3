4a714efbd4dd1617c3bd4a154d25b72cf0a0b6fd
Hustra03 <erik.paulinder@gmail.com>
2021-01-27 11:07:17 +0100

    Börjat på java

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,14 @@
-let function name(lapp) {
-    
+function CreateNote(params) {
+    let text = document.getElementById('textbox').value;
+    var para = document.createElement("P");
+    var t = document.createTextNode("This is a paragraph.");
+    para.appendChild(t);
+    document.getElementById('Lappar').appendChild(newContent);
+    const help = document.getElementById("help");
+    const textbox = document.getElementById("textbox");
+    document.body.insertBefore(help,textbox);
+
+
+
+
 }
