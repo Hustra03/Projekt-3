8235a6b4d74fd0c13ef9570d3659b27db0121efd
Hustra03 <erik.paulinder@gmail.com>
2021-02-12 13:16:46 +0100

    Local Storage Fungerar

--- javascript/main.js
+++ javascript/main.js
@@ -2,54 +2,45 @@
 
 const LOCAL_STORAGE_KEY_TODOS =  "app.todos";
 let todos  = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TODOS)) || [];
-//let todos = "";
 let listRoot = document.getElementById('list');
 let x=0;
+listRoot.addEventListener("click", DeleteNote)
 
 
-function CreateNote() {
-    let text = document.getElementById('textbox').value;
+function CreateNote(text) {
     if (text.trim() != "")
     {
         let todoListItem = document.createElement("li");
-        let list = document.getElementById('list');
         todoListItem.innerText = text;
         todoListItem.classList.add("textitem");
-        todoListItem.addEventListener("click", DeleteNote)
-        list.append(todoListItem);
+        listRoot.append(todoListItem)
+        todos = listRoot.innerHTML;
         updateList();
     }
 }
-
-
-function todoList(items) {
-    let list = document.createElement("ul");
-    items.forEach((item) => {
-      let todoListItem = document.createElement("li");
-      todoListItem.innerText = item;
-      todoListItem.classList.add("todo-list-item");
-      todoListItem.addEventListener("click", removeItem);
-      list.append(todoListItem);
-    });
-    return list;
-  }
 
 function DeleteNote(event)
 {
     let node = document.getElementById("list");
     let child = event.target;
     node.removeChild(child);
+    todos = listRoot.innerHTML;
     updateList();
-    
-}
+  }
+
+  function createElementFromHTML(htmlString) {
+    listRoot.innerHTML = htmlString;
+  }
 
 function updateList() {
     saveList();
-    console.log(localStorage)
-    todos = document.getElementById('list').innerHTML
-    listRoot.append(todoList(todos));
+    console.log(todos)
+    createElementFromHTML(todos)
+
   }
   
   function saveList() {
     localStorage.setItem(LOCAL_STORAGE_KEY_TODOS, JSON.stringify(todos));
   }
+
+updateList()
